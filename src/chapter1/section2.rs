0c57dd16b1087b5a3dde12ce02dab12b30c1313f
imkiva <libkernelpanic@gmail.com>
2019-12-03 23:04:34 +0800

    rust: chapter1 section2: vector printing

--- src/chapter1/section2.rs
+++ src/chapter1/section2.rs
@@ -190,3 +190,41 @@ pub fn display_printable() {
     println!("Display: {}", complex);
     println!("Debug: {:?}", complex);
 }
+
+// Code exercise from
+// https://doc.rust-lang.org/stable/rust-by-example/hello/print/print_display/testcase_list.html
+// Activity:
+// Try changing the program so that the index of each element in the vector is also printed.
+// The new output should look like this:
+// [0: 1, 1: 2, 2: 3]
+pub fn vector_printable() {
+    use std::fmt; // Import the `fmt` module.
+
+    // Define a structure named `List` containing a `Vec`.
+    struct List(Vec<i32>);
+
+    impl fmt::Display for List {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Extract the value using tuple indexing,
+            // and create a reference to `vec`.
+            let vec = &self.0;
+
+            write!(f, "[")?;
+
+            // Iterate over `v` in `vec` while enumerating the iteration
+            // count in `count`.
+            for (count, v) in vec.iter().enumerate() {
+                // For every element except the first, add a comma.
+                // Use the ? operator, or try!, to return on errors.
+                if count != 0 { write!(f, ", ")?; }
+                write!(f, "{}: {}", count, v)?;
+            }
+
+            // Close the opened bracket and return a fmt::Result value.
+            write!(f, "]")
+        }
+    }
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+}
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,5 @@ fn main() {
     chapter1::section2::formatted_print();
     chapter1::section2::debug_printable();
     chapter1::section2::display_printable();
+    chapter1::section2::vector_printable();
 }
